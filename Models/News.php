2600669d589c5d7d569c3439ed5f081f1928b15b
Oleg <oarch@mail.ru>
2015-03-12 20:25:51 +0300

    Модели работы с БД

---
+++ Models/News.php
@@ -0,0 +1,17 @@
+<?php
+
+
+namespace Application\Models;
+
+/**
+ * Class NewsModel
+ * @property $id
+ * @property $title
+ * @property $text
+ */
+
+class News
+      extends \AbstractModel
+{
+     protected static $table = 'news';
+}
